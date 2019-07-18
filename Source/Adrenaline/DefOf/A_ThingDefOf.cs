d0d26dc99e2e71c4f241db08ed435dde0ea133ff
XeoNovaDan <toghill7252010@hotmail.co.uk>
2019-07-18 20:30:58 +0100

    Added adrenaline syringe

---
+++ Source/Adrenaline/DefOf/A_ThingDefOf.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+using RimWorld;
+using RimWorld.Planet;
+
+namespace Adrenaline
+{
+
+    [DefOf]
+    public static class A_ThingDefOf
+    {
+
+        public static ThingDef Adrenaline;
+
+    }
+
+}
---
+++ Source/Adrenaline/HarmonyPatches/Patch_Pawn.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+using RimWorld;
+using RimWorld.Planet;
+using Harmony;
+
+namespace Adrenaline
+{
+
+    public static class Patch_Pawn
+    {
+
+        [HarmonyPatch(typeof(Pawn))]
+        [HarmonyPatch(nameof(Pawn.GetGizmos))]
+        public static class Patch_GetGizmos
+        {
+
+            public static void Postfix(Pawn __instance, ref IEnumerable<Gizmo> __result)
+            {
+                // If the pawn is downed, is a colonist and can take adrenaline, add a 'take adrenaline' gizmo
+                if (__instance.Downed && __instance.IsColonistPlayerControlled && __instance.def.CanGetAdrenaline())
+                {
+                    var adrenalineCells = __instance.CellsAdjacent8WayAndInside().Where(c => c.GetFirstThing(__instance.Map, A_ThingDefOf.Adrenaline) != null);
+                    var adrenalineGizmo = new Command_Action()
+                    {
+                        defaultLabel = "Adrenaline.Command_TakeAdrenaline".Translate(),
+                        defaultDesc = "Adrenaline.Command_TakeAdrenaline_Description".Translate(),
+                        icon = ContentFinder<Texture2D>.Get(A_ThingDefOf.Adrenaline.graphicData.texPath),
+                        action = () => adrenalineCells.First().GetFirstThing(__instance.Map, A_ThingDefOf.Adrenaline).Ingested(__instance, 0)
+                    };
+
+                    // No adrenaline nearby
+                    if (!adrenalineCells.Any())
+                        adrenalineGizmo.Disable("Adrenaline.Command_TakeAdrenaline_FailNoAdrenaline".Translate());
+
+                    // Can't do manipulation
+                    else if (!__instance.health.capacities.CapableOf(PawnCapacityDefOf.Manipulation))
+                        adrenalineGizmo.Disable("Adrenaline.Command_TakeAdrenaline_FailNoManipulation".Translate());
+
+                    __result = __result.Add(adrenalineGizmo);
+                }
+            }
+
+        }
+
+    }
+
+}
